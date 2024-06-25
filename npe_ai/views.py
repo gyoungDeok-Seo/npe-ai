34dfc73517af74e3e3a2387ce8f908964117fdd6
baek-si-hyun <qortlgus100@gmail.com>
2024-06-25 09:31:36 +0900

    240625 ai done

--- npe_ai/views.py
+++ npe_ai/views.py
@@ -23,3 +23,32 @@ class ReplyFilterAPI(APIView):
             joblib.dump(model, model_file_path)
 
         return Response(prediction[0])
+
+
+class AnswerReportAPI(APIView):
+    def get(self, request):
+        answer_content = request.GET.get('answer-content')
+
+        model_file_path = os.path.join(Path(__file__).resolve().parent, 'ai/reply_default_model.pkl')
+        model = joblib.load(model_file_path)
+        X_train = [answer_content]
+
+        transformed_X_train = model.named_steps['count_vectorizer'].transform(X_train)
+        model.named_steps['multinomial_NB'].partial_fit(transformed_X_train, [1])
+        joblib.dump(model, model_file_path)
+
+        return Response('ok')
+
+class ReplyReportAPI(APIView):
+    def get(self, request):
+        reply_content = request.GET.get('reply-content')
+
+        model_file_path = os.path.join(Path(__file__).resolve().parent, 'ai/reply_default_model.pkl')
+        model = joblib.load(model_file_path)
+        X_train = [reply_content]
+
+        transformed_X_train = model.named_steps['count_vectorizer'].transform(X_train)
+        model.named_steps['multinomial_NB'].partial_fit(transformed_X_train, [1])
+        joblib.dump(model, model_file_path)
+
+        return Response('ok')
